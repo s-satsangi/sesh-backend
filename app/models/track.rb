dc452872f94c38d8f4f832f2cf852b779935e70c
s-satsangi <s.satsangi@gmail.com>
2020-12-01 21:49:21 -0500

    added extra column for carrierwave

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,4 +1,4 @@
 class Track < ApplicationRecord
-    mount_uploader :link, TrackUploader
+    mount_uploader :bucket_link, TrackUploader
     # https://nayan.co/blog/Ruby-on-Rails/how-to-upload-files-using-carrierwave-google-storage/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_01_170404) do
+ActiveRecord::Schema.define(version: 2020_12_02_024624) do
 
   create_table "songs", force: :cascade do |t|
     t.string "title"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2020_12_01_170404) do
     t.string "link"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "bucket_link"
   end
 
   add_foreign_key "track_songs", "songs"
