e797ca4cb45180caeb29794e06dea3d5e9e63701
s-satsangi <s.satsangi@gmail.com>
2020-12-03 16:55:36 -0500

    nuked some extra comments

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,11 +5,6 @@ CarrierWave.configure do |config|
   config.gcloud_authenticated_url_expiration = 600
   config.gcloud_content_disposition          = 'attachment'          # or you can skip this
   
-  # config.fog_credentials = {
-  # }
-  
-  # config.fog_directory = "ducks-and-crackers"
-  
   config.gcloud_attributes = {
     expires: 600
   }
@@ -17,10 +12,6 @@ CarrierWave.configure do |config|
   config.gcloud_credentials = {
     gcloud_project: 'sesh-297310',                              # Better fetch this from environment variables
     gcloud_keyfile: '/mnt/c/Users/ssats/Documents/GitHub/Phase3/project/Sesh/sesh-297301-646c2a16a5e6.json'                       # Better fetch this from environment variables
-    # gcloud_keyfile: '63aa6d2c3964aa0751556f7a196f4884fc161217',
-    # provider:                         'Google'
-    # google_storage_access_key_id:     "GOOG3RNEYZ4F6XTDJT53VCVO",
-    # google_storage_secret_access_key: "8fyCxoHG8iF3xISsr5nVlADu/gXfU1fPPelWqcok"
   }
 end
 # ^^^^ the above 16 lines are adapted from
